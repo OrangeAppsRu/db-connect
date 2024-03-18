b2a108b9689997d0f291cfa522d697be1616b908
Dmitry Sergeev <identw@gmail.com>
2024-03-18 16:35:51 +0400

    fixed entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
- #!/usr/bin/env bash
+#!/usr/bin/env bash
 export PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin
 cp -fv /user/.bash_profile ${HOME}/.bashrc
 echo "" > ${HOME}/.sudo_as_admin_successful
